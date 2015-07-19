6fa53b820e8fbd9b6029a53abc95c877307b6004
Christopher Findeisen <cfindeisen7@gmail.com>
2015-07-19 15:45:56 -0500

    whitespace

--- init.el
+++ init.el
@@ -42,7 +42,7 @@
   (loop for p in required-packages
         when (not (package-installed-p p)) do (return nil)
         finally (return t)))
- ;if not all packages are installed, check one by one and install the missing ones.
+; if not all packages are installed, check one by one and install the missing ones.
 (unless (packages-installed-p)
    check for new packages (package versions)
   (message "%s" "Emacs is now refreshing its package database...")
