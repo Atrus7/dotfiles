0c8679b0fd3323f3ffba36813cd1cad0e910c63d
Atrus7 <cfindeisen7@gmail.com>
2015-08-17 12:37:01 -0500

    Mac customizations in it's own file

---
+++ mac.el
@@ -0,0 +1,13 @@
+(setq mac-command-key-is-meta t) ; apple = meta
+(setq mac-pass-command-to-system nil) ; avoid hiding with M-h
+
+;; Ignore .DS_Store files with helm mode
+(add-to-list 'helm-boring-buffer-regexp-list "\\.DS_Store$")
+
+;; Open files
+(defun mac-open-current-file ()
+  (interactive)
+  (shell-command (concat "open " (buffer-file-name))))
+
+(provide 'mac)
+;;; mac.el ends here
