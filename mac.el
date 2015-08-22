7fe4616ad06efbbd7c65f94209cb6ed56eb93ea4
Christopher Findeisen <cfindeisen7@gmail.com>
2015-08-22 11:57:08 -0500

    Moved logic out of this into init

--- mac.el
+++ mac.el
@@ -1,13 +1,4 @@
-(setq mac-command-key-is-meta t) ; apple = meta
-(setq mac-pass-command-to-system nil) ; avoid hiding with M-h
-
-;; Ignore .DS_Store files with helm mode
-(add-to-list 'helm-boring-buffer-regexp-list "\\.DS_Store$")
-
-;; Open files
-(defun mac-open-current-file ()
-  (interactive)
-  (shell-command (concat "open " (buffer-file-name))))
+(message "You're on a mac")
 
 (provide 'mac)
 ;;; mac.el ends here
