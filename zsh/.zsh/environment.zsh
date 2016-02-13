e3cdbc2175fe1916b1bf38b28fd5433b6cd529a1
Christopher Findeisen <cfindeisen7@gmail.com>
2016-02-13 09:40:11 -0600

    Frezsh start :D

---
+++ zsh/.zsh/environment.zsh
@@ -0,0 +1,22 @@
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin
+
+export EDITOR=emacs
+export VISUAL=emacs
+export ALTERNATE_EDITOR=vim
+
+#languages
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
+export LC_MONETARY=en_US.UTF-8
+export LC_NUMERIC=en_US.UTF-8
+export LC_TIME=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LESSCHARSET=utf-8
+
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+export CC="`which gcc-4.9` -fdiagnostics-color=auto"
+export CXX="`which g++-4.9` -fdiagnostics-color=auto"
