e6f6639caa9f03084d17a0cd617895ebf7102c42
Christopher Findeisen <cfindeisen7@gmail.com>
2016-09-03 15:59:27 -0500

    Don't have a configure-linux in spacemacs

--- spacemacs/.spacemacs.d/private/cf-linux/config.el
+++ spacemacs/.spacemacs.d/private/cf-linux/config.el
@@ -4,18 +4,20 @@
       (progn
         (message "Server Configuring.")
         (setq server-log 1)
-        (add-hook 'after-make-frame-functions 'cf/server-configure t))
-    (progn
-      (message "Regular Configuring.")
-      (cf/configure-os-linux))
-    )
+        (add-hook 'after-make-frame-functions 'cf/server-configure t)
 
-  ;; How do we do this without ruining buffers?
-  (when (daemonp)
-    (defadvice my-desktop-restore-file-buffer-advice
-        (around my-desktop-restore-file-buffer)
-      "Be non-interactive while starting a daemon."
-      (let ((noninteractive t))
-        ad-do-it))
-    (ad-activate 'my-desktop-restore-file-buffer-advice))
+        ;; How do we do this without ruining buffers?
+        (defadvice my-desktop-restore-file-buffer-advice
+            (around my-desktop-restore-file-buffer)
+          "Be non-interactive while starting a daemon."
+          (let ((noninteractive t))
+            ad-do-it))
+        (ad-activate 'my-desktop-restore-file-buffer-advice)))
+
+
+
+  (progn
+    (message "Regular Configuring.")
+    )
   )
+
