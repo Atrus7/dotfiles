532818d9a17e9e78b64cf04fbfaf1ccd368e2663
Christopher Findeisen <cfindeisen7@gmail.com>
2016-12-22 02:17:41 -0600

    add scheme thing to always babel up scheme code

--- spacemacs/.spacemacs.d/private/cf-scheme/config.el
+++ spacemacs/.spacemacs.d/private/cf-scheme/config.el
@@ -1,3 +1,7 @@
+(add-to-list 'org-src-lang-modes '("scheme" . scheme) )
+(add-to-list 'org-babel-load-languages '(scheme . t))
+(org-babel-do-load-languages 'org-babel-load-languages '((emacs-lisp . t) (scheme . t) ))
+
 (when (spacemacs/system-is-linux)
   (setq scheme-program-name "guile")
   )
