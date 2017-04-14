ae09aa2d595500285d6ae5769a54849ccaa21851
Christopher Findeisen <cfindeisen7@gmail.com>
2017-04-14 10:15:26 -0500

    getting p to set priority

---
+++ spacemacs/.spacemacs.d/private/cf-org/keybindings.el
@@ -0,0 +1,6 @@
+;; remapping pomodoro thing
+(spacemacs/set-leader-keys-for-major-mode 'org-mode
+  "p" 'org-priority)
+(spacemacs/set-leader-keys-for-major-mode 'org-agenda-mode
+  "p" 'org-agenda-priority)
+
