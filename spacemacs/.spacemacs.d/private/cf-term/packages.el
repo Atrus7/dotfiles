f4256ba978b563bff9f5f7f235d7db3ba0da2241
Atrus7 <cfindeisen7@gmaila.com>
2018-04-01 22:43:46 -0700

    win movement even in term-raw and org mode...

---
+++ spacemacs/.spacemacs.d/private/cf-term/packages.el
@@ -0,0 +1,16 @@
+(defconst cf-term-packages '(term)
+  "The list of Lisp packages required by the cf-term layer.")
+
+(defun cf-term/post-init-term()
+  (use-package term
+    :defer t
+    :config
+    (evil-define-key '(normal insert) term-raw-map
+      (kbd "M-x") 'helm-M-x
+
+      (kbd "M-h") 'evil-window-left
+      (kbd "M-j") 'evil-window-down
+      (kbd "M-k") 'evil-window-up
+      (kbd "M-l") 'evil-window-right)
+    )
+  )
