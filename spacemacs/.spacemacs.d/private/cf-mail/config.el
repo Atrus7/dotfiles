6fc3bf05cd3e1abe5aa17e87a21fe6ced863b457
Chris Findeisen <cfindeisen7@gmail.com>
2018-07-29 23:29:06 -0700

    Trying MU4E

--- spacemacs/.spacemacs.d/private/cf-mail/config.el
+++ spacemacs/.spacemacs.d/private/cf-mail/config.el
@@ -24,6 +24,19 @@
   (gnus-agent-toggle-plugged nil))
 
 
+
+(setq
+ ;; Tell message mode to use SMTP.
+ send-mail-function		nil
+ message-send-mail-function	'smtpmail-send-it
+ smtpmail-default-smtp-server "smtp.gmail.com"
+
+ ;; This tells Gnus to use the Gmail SMTP server. This
+ ;; automatically leaves a copy in the Gmail Sent folder.
+ smtpmail-smtp-service 587)
+
+(setq nntp-authinfo-file "~/.authinfo.gpg")
+
 ;; (setq gnus-parameters '((".*" (banner . iphone)))
 ;; gnus-article-banner-alist '((iphone . "\\(^Sent from my iPhone$\\)"))
 ;; gnus-use-full-window nil)
