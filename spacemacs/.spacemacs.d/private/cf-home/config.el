fe82e2ee3c73d7684ec6775c8cc8bc29f10fa8a3
Chris Findeisen <cfindeisen7@gmail.com>
2018-07-29 23:29:25 -0700

    Update emacs with latest and universalize delete window

--- spacemacs/.spacemacs.d/private/cf-home/config.el
+++ spacemacs/.spacemacs.d/private/cf-home/config.el
@@ -1,5 +1,5 @@
 ;; To be the default opinions of my home setup. Particular to outside programs that are installed on my machine other than emacs.
-(setq browse-url-browser-function 'browse-url-chromium)
+(setq browse-url-browser-function 'browse-url-chrome)
 
 ;; gnus stuff
 (setq
