9a5541fd465cf04bd6028586f13d6fc939959375
Chris Findeisen <cfindeisen7@gmail.com>
2018-11-24 17:33:44 -0800

    ssh into machines..

--- zsh/.zsh/ssh.zsh
+++ zsh/.zsh/ssh.zsh
@@ -1,4 +1,9 @@
 export SSH_KEY_PATH="~/.ssh/"
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+# gpgconf --launch gpg-agent
 # no keychain on mac
 if type keychain>/dev/null; then
  eval `keychain --eval id_rsa`
