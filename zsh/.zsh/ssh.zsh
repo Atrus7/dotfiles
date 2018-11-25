c49ad670261563345f6929418bb342d2cfd1b222
Atrus7 <cfindeisen7@gmaila.com>
2018-11-24 17:15:18 -0800

    Sshing into home machine

--- zsh/.zsh/ssh.zsh
+++ zsh/.zsh/ssh.zsh
@@ -1,5 +1,8 @@
 export SSH_KEY_PATH="~/.ssh/"
 # no keychain on mac
-if type keychain; then
- eval `keychain --eval id_rsa`
-fi
+export GPG_TTY="$(tty)"
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+# if type keychain; then
+#  eval `keychain --eval id_rsa`
+# fi
