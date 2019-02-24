16dc02c30839f2f67b27e425b214697e0a71d0e1
Chris Findeisen <cfindeisen7@gmail.com>
2019-02-24 10:24:38 -0800

    CF writing developed

---
+++ linux/bin/vlc_control.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import subprocess
+import time
+import threading
+
+import sys
+import telnetlib
+
+class VideoInfo():
+    def __init__(self, v_id, v_time):
+        self.v_id = v_id
+        self.v_time = v_time
+
+class VLCTelnet():
+
+    HOST = "localhost"
+    VIDEO_INFO_FILE = "/home/pi/video_info"
+
+    def __init__(self):
+        "Signs in"
+        self.open_telnet_buffer()
+
+    def write(self, message):
+        self.tn.write(message.encode('ascii') + b"\n")
+
+    def open_telnet_buffer(self):
+        self.tn = telnetlib.Telnet(VLCTelnet.HOST, 4212, 30)
+        self.tn.read_until(b"Password: ")
+        self.write("admin")
+        self.tn.read_until(b"> ")
+
+    def clear_telnet_buffer(self):
+        """hacky...just close and reopen"""
+        self.tn.close()
+        self.open_telnet_buffer()
+
+    def print_video_time(self):
+        self.tn.read_until(b"\n", .5)
+        self.write("get_time")
+        tmp_time = self.tn.read_until(b"\n", .5)
+        try:
+            total_seconds=int(tmp_time)
+
+            minutes = total_seconds // 60
+            seconds = total_seconds % 60
+            print(str(minutes) + ":" + str(seconds))
+
+        except ValueError:
+            print("Invalid current time " + tmp_time.decode('ascii'))
+
+    def __del__(self):
+        self.write("quit")
+
+TELNET = VLCTelnet()
+
+TELNET.write("pause")
+TELNET.print_video_time()
