34b991fc7bbcb04471bc2d76334842ee318a86b0
Chris Findeisen <atrus@desk>
2021-06-15 20:22:11 -0700

    Cabal sucks, noted.

--- linux/bin/no-attrs-filter.hs
+++ linux/bin/no-attrs-filter.hs
@@ -1,3 +1,6 @@
+#!/usr/bin/env runhaskell
+
+-- NOTE TO SELF. Never try to fuck with cabal install. Follow the stackexchange advice and just install pandoc libs through debian.
 -- From this stackoverflow answer: https://emacs.stackexchange.com/a/54408
 -- The purpose is to remove extraneous "PROPERTIES"
 import Text.Pandoc.JSON
