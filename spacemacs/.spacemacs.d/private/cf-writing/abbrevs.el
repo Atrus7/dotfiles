d9daf876918d09c1cfa20a615071969b80c7d472
Chris Findeisen <atrus@desk>
2021-07-28 22:25:18 -0500

    abbrev-update

--- spacemacs/.spacemacs.d/private/cf-writing/abbrevs.el
+++ spacemacs/.spacemacs.d/private/cf-writing/abbrevs.el
@@ -3,6 +3,10 @@
 (define-abbrev-table 'org-mode-abbrev-table '(
     ("siad" "said" nil 0)
     ("thta" "that" nil 0)
+    ("htis" "this" nil 0)
+    ("iwth" "with" nil 0)
+    ("coupel" "couple" nil 0)
     ("teh" "the" nil 0)
-    ("--" "—" nil 0)
+    ("jsut" "just" nil 0)
+    ("--" "—" nil 0) ;; not working
 ))
