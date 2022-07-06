26f33b7576b51247e998b680857326b58e7a16bf
Chris Findeisen <atrus@desk>
2022-07-06 09:04:29 -0500

    python reqs

--- spacemacs/.spacemacs.d/private/cf-py/config.el
+++ spacemacs/.spacemacs.d/private/cf-py/config.el
@@ -1,1 +1,2 @@
 ;; (add-hook python-mode-hook (lambda() (unbind-key "TAB" evil-normal-state-map)))
+(add-to-list 'exec-path (expand-file-name "~/.local/bin")) ;; Where pip installs certain packages.
