a62b0a5c39ed821fe4cd31063702987b144e4924
Chris Findeisen <atrus@desk>
2025-05-15 17:57:45 -0500

    Fix history

--- zsh/.zsh/history.zsh
+++ zsh/.zsh/history.zsh
@@ -1,7 +1,24 @@
 HISTFILE=~/.zhistory
-setopt APPEND_HISTORY
-HISTSIZE=1200
-SAVEHIST=1000
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt EXTENDED_HISTORY
-setopt SHARE_HISTORY
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+# Make sure history from all sessions is shared and appended
+setopt APPEND_HISTORY             # Don't overwrite history
+setopt SHARE_HISTORY              # Share with other sessions
+setopt HIST_IGNORE_ALL_DUPS       # Avoid clutter from duplicates
+setopt HIST_REDUCE_BLANKS         # Remove unnecessary whitespace
+setopt EXTENDED_HISTORY           # Include timestamp and duration
+
+alias history='fc -rl 1' # so i can do things like history | grep ssh
+
+
+# Ctrl+R: reverse-i-search using fzf
+autoload -Uz up-line-or-beginning-search
+bindkey '^R' fzf-history-widget
+
+fzf-history-widget() {
+    BUFFER=$(fc -rl 1 | fzf --tac | sed 's/^[ 0-9]*//')
+    CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N fzf-history-widget
